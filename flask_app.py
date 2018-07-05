c73d18fa76aa18ef2c0b7bcd00371924521510bf
Alicia Gilbert <40601091+itsimplified@users.noreply.github.com>
2018-07-05 15:26:22 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,6 @@ import sys
 sys.path.insert(1, "PATH TO LOCAL PYTHON PACKAGES")  #OPTIONAL: Only if need to access Python packages installed on a local (non-global) directory
 sys.path.insert(2, "PATH TO FLASK DIRECTORY")      #OPTIONAL: Only if you need to add the directory of your flask app
 
-
 app = Flask(__name__)
 
 @app.route('/') 
