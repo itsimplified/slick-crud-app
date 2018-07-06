3be8cbdc49f669a6308c5aa3034d0c0332c246b3
Alicia Gilbert <40601091+itsimplified@users.noreply.github.com>
2018-07-06 11:04:41 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, redirect, render_template
-import re
 import sys
 sys.path.insert(1, "PATH TO LOCAL PYTHON PACKAGES")  #OPTIONAL: Only if need to access Python packages installed on a local (non-global) directory
 sys.path.insert(2, "PATH TO FLASK DIRECTORY")      #OPTIONAL: Only if you need to add the directory of your flask app
